25b5d76d6250b37f058767f33a8c7669381bc8fd
Sophat Peou <sophat.peou@gmail.com>
2019-10-07 18:18:20 -0700

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
+
+const app = express();
+
+mongoose.connect("mongodb+srv://sopa:Megaman69!@cluster0-spcvx.mongodb.net/node-angular?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then( () => {
+    console.log('Connected to database!');
+  }).catch( () => {
+    console.log('Connection Failed!');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use((req, res, next)=>{
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, " +
+    "Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS");
+  next();
+
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added successfully",
+      postId: createdPost._id
+    });
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
+  Post.find().then(documents =>{
+    res.status(200).json({
+      message: "Post fetched successfully!",
+      posts: documents
+    });
+  });
+});
+
+// app.use('/api/posts', (req, res, next)=> {
+//   const posts = [];
+//   return res.status(200).json({
+//     message: 'post successful!',
+//     posts: posts
+//   });
+// });
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
+  });
+});
+
+module.exports = app;
